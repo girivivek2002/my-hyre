e6e2d9997645e08535d94eba7d0a696be5b412f9
royalrajputayush <singh18ayush@gmail.com>
2026-04-23 17:07:26 +0530

    fix: add AuthProvider to root layout to fix useSession prerendering error

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,14 +9,17 @@ export const metadata = {
 };
 
 import { ThemeProvider } from "../components/ThemeProvider";
+import { AuthProvider } from "../components/AuthProvider";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        <AuthProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   );
